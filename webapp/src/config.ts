71a6ba65debc8a511015cd2e36cb52bebae1d213
Michael Elliott <kllmmc23@gmail.com>
2024-11-29 15:11:21 -0700

    Webapp: Restore paths

--- webapp/src/config.ts
+++ webapp/src/config.ts
@@ -2,10 +2,10 @@ const Config = {
     API_ENDPOINT: import.meta.env.VITE_API_ENDPOINT,
     LINKS: {
         HOME: "/",
-        APPLICATIONS_LISTING: "/api/applications",
-        APPLICATION: (id: string): string => `/api/applications/${id}`,
-        LOGIN: "/api/login",
-        REGISTER: "/api/register"
+        APPLICATIONS_LISTING: "/applications",
+        APPLICATION: (id: string): string => `/applications/${id}`,
+        LOGIN: "/login",
+        REGISTER: "/register"
     }
 }
 
